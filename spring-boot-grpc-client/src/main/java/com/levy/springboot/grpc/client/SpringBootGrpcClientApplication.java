189869e47b3c04e70b6beb877606396888d1af71
Xia Lin <hero0567@163.com>
2021-10-15 15:14:30 +0800

    client and server work

--- spring-boot-grpc-client/src/main/java/com/levy/springboot/grpc/client/SpringBootGrpcClientApplication.java
+++ spring-boot-grpc-client/src/main/java/com/levy/springboot/grpc/client/SpringBootGrpcClientApplication.java
@@ -1,13 +1,27 @@
 package com.levy.springboot.grpc.client;
 
+import com.levy.springboot.grpc.client.service.GrpcClientService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class SpringBootGrpcClientApplication {
+public class SpringBootGrpcClientApplication implements CommandLineRunner {
+
+	@Autowired
+	private GrpcClientService grpcClientService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGrpcClientApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(">>>>>>>>>>>>>>>>>服务启动执行");
+		String result = grpcClientService.sendMessage("11");
+		log.info("Server response message: {}", result);
+	}
 }
--- spring-boot-grpc-server/src/main/java/com/levy/springboot/grpc/server/service/GrpcServerService.java
+++ spring-boot-grpc-server/src/main/java/com/levy/springboot/grpc/server/service/GrpcServerService.java
@@ -4,17 +4,19 @@ import com.levy.springboot.grpc.lib.HelloReply;
 import com.levy.springboot.grpc.lib.HelloRequest;
 import com.levy.springboot.grpc.lib.SimpleGrpc;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 /**
  * @Auther: Lin.Xia
  * @Date: 2019/3/6 18:15
  */
+@Slf4j
 @GrpcService
 public class GrpcServerService extends SimpleGrpc.SimpleImplBase {
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        System.out.println("GrpcServerService...");
+        log.info("Receive client message from sayHello: {}", request.getName());
         HelloReply reply = HelloReply.newBuilder().setMessage("Hello ==> " + request.getName()).build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
